322beda54a7c2cf10630d51e5b2ea395fad9da71
Richard Cui <cuir@uoguelph.ca>
2024-07-24 17:36:38 -0400

    SAVE: Saving changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
