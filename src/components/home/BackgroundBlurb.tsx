0eae3866648ff268df3a0464e71e97ffeaa69e6c
Richard Cui <cuir@uoguelph.ca>
2024-08-03 03:32:22 -0400

    WIP: Fade in/out animation works for first image. Rest are ignored
    So it's re-rendering and it's getting the right direction. The fadeType thing is triggering a rerender which changes the animation tag. But it's just ignoring it. Will look into it further

--- src/components/home/BackgroundBlurb.tsx
+++ src/components/home/BackgroundBlurb.tsx
@@ -1,9 +1,19 @@
-export default function Blurb({ width, photo }: { width: string, photo: string }) {
+import { useEffect, useState } from "react"
+
+import { fadeType } from "@/lib/constants"
+
+export default function Blurb({ width, photo, fade }: { width: string, photo: string, fade: fadeType }) {
+    const [fadeType, setFadeType] = useState("in" as fadeType)
+
+    useEffect(() => {
+        setFadeType(fade)
+    })
+
     return (
         <div style={{ width }}>
             <svg
-                width="850"
-                height="650"
+                width={width}
+                // height="650"
                 viewBox="0 0 224.89583 171.97917"
                 version="1.1"
                 id="svg1"
@@ -18,7 +28,9 @@ export default function Blurb({ width, photo }: { width: string, photo: string }
                         d="m 0,116.11938 c 15.47073,38.23227 31.830581,31.65276 31.830581,31.65276 0,0 7.646453,-1.77824 17.960274,-8.7134 10.31382,-6.93515 9.424696,21.51677 31.830581,28.2741 22.405884,6.75733 29.696694,4.08996 33.786654,3.91213 4.08996,-0.17782 28.45191,-9.42469 37.16531,-25.60672 8.7134,-16.18203 13.33684,-13.69249 13.33684,-13.69249 0,0 14.58161,9.4247 26.8515,10.31383 12.26989,0.88912 30.76363,-9.60253 19.56069,-58.682084 C 201.11949,34.497949 205.5651,38.054438 211.07766,33.608827 216.59022,29.163216 226.19274,32.364055 224.4145,20.271991 222.63625,8.1799262 214.63415,-0.35564897 205.03163,-1.7782448 195.4291,-3.2008408 -6.7573302,-4.089963 -6.7573302,-4.089963 Z"
                         id="path1"
                         fill="url(#imagePattern)"
-                    />
+                    >
+                        {fadeType === "in" ? <animate id="in" attributeType="CSS" attributeName="opacity" from="0" to="1" dur="1s" repeatCount={1} /> : <animate id="out" attributeType="CSS" attributeName="opacity" from="1" to="0" dur="6s" repeatCount={1} />}
+                    </path>
                 </g>
             </svg>
         </div>
--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -2,37 +2,39 @@
 
 import { useState } from "react";
 
-import Typed from "typed.js";
-
-import styles from "@/styles/home/HomePage.module.css"
-
 import Image from "next/image";
 import Link from "next/link"
 
+import styles from "@/styles/home/HomePage.module.css"
 import BackgroundBlurb from "@/components/home/BackgroundBlurb";
 import Title from "@/components/home/Title";
-import { getPhotos } from "@/lib/mediaDefinitions";
+import { fadeType } from "@/lib/constants";
 
 
 export default function HomePage() {
-    const [photo, setPhoto] = useState('profile.jpg')
+    const [photo, setPhoto] = useState('')
+    const [fade, setFade] = useState('in' as fadeType)
 
     const changePhoto = (photoLocation: string) => {
         setPhoto(photoLocation)
     }
 
+    const changeFade = (fadeType: fadeType) => {
+        setFade(fadeType)
+    }
+
     return (
         <section className="grid grid-cols-2">
-            <BackgroundBlurb width="100%" photo={photo} />
-            <HomePageContents changePhoto={changePhoto}/>
+            <BackgroundBlurb width="100%" photo={photo} fade={fade}/>
+            <HomePageContents changePhoto={changePhoto} changeFade={changeFade}/>
         </section>
     )
 }
 
-function HomePageContents({changePhoto}: {changePhoto: (photoLocation: string) => void}) {
+function HomePageContents({changePhoto, changeFade}: {changePhoto: (photoLocation: string) => void, changeFade: (fadeType: fadeType) => void}) {
     return (
         <article className="px-16 flex flex-col place-content-center gap-16 text-default">
-            <Title changePhoto={changePhoto} />
+            <Title changePhoto={changePhoto} changeFade={changeFade}/>
             <div className={styles.bio}>
                 <p>I'm...</p>
                 <ul className={styles.list}>
