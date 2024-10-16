1a48d129bd2e7ac4c46e38997e04ac82dd4829dc
richacui70 <richacui70@gmail.com>
2024-10-15 21:42:36 -0400

    NEW: Added new section:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import MyNavBar from "@/components/navbar/NavBar";
 import HomePage from "@/components/home/HomePage";
 import ProjectsPage from "@/components/projects/ProjectsPage";
 import ProfilePage from "@/components/profile/ProfilePage"
+import OuttakesPage from "@/components/outtakes/OuttakesPage"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HomePage />
       <ProjectsPage />
       <ProfilePage />
+      <OuttakesPage />
     </>
   );
 }
---
+++ src/components/outtakes/OuttakesPage.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/outtakes/outtakes.module.css"
+
+export default function OuttakesPage() {
+    return (
+        <section id="profile" className={styles.container}>
+            <h2>Outtakes.</h2>
+        </section>
+    )
+}
